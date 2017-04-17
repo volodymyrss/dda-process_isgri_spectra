70f582eb17d558715b719c01470c4aa84a582a10
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-04-17 16:48:46 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='proccess_isgri_spectra',
+    version='1.0',
+    py_modules=["proccess_isgri_spectra"],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
